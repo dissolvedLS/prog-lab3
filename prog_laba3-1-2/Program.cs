6726d0b445cf0dc5622bd08b72b9e751067574dd
dissolvedLS <deodex@spaces.ru>
2019-05-28 13:48:55 +0300

    Добавьте файлы проекта.

---
+++ prog_laba3-1-2/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace prog_laba3_1_2
+{
+    class Program
+    {
+        static void Main()
+        {
+            Console.Write("n=");
+            int n = int.Parse(Console.ReadLine());
+            int[] myArray = new int[n];
+            int i;
+            for (i = 0; i < n; i++)
+                myArray[i] = i * i;
+            for (i = 0; i < n; i++)
+                Console.Write(myArray[i] + " ");
+        }
+    }
+}
